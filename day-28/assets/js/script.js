40748fb8172a8a6dc6b1379fdd5f18fbdf9392a3
nhunglt1801 <nhunglt1801@gmail.com>
2024-07-01 00:59:50 +0700

    Update Second Commit JS

--- day-28/assets/js/script.js
+++ day-28/assets/js/script.js
@@ -4,78 +4,69 @@ var btnAdd = document.querySelector(".add-btn");
 document.addEventListener("DOMContentLoaded", function () {
   inputBox.focus();
 });
+
 function addTask(e) {
   var input = e.target.parentElement.querySelector(".input-box");
-  var inputValue = input.value;
+  var inputValue = input.value.trim();
   if (!inputValue) {
     input.focus();
     return;
-  } else {
-    var li = document.createElement("li");
-    li.classList.add("list-item");
-    li.innerHTML = `
+  }
+  var li = document.createElement("li");
+  li.classList.add("list-item");
+  li.innerHTML = `
     <span class="text">${inputValue}</span>
     <i class="fa-solid fa-pen-to-square edit-btn"></i>
     <i class="fa-solid fa-trash delete-btn"></i>`;
-    listContainer.appendChild(li);
-  }
+  listContainer.appendChild(li);
   input.value = "";
   input.focus();
 }
-btnAdd.addEventListener("click", function (e) {
-  addTask(e);
-});
-inputBox.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    addTask(e);
-  }
-});
 
-listContainer.addEventListener("click", function (e) {
-  if (e.target.classList.contains("fa-trash")) {
-    e.target.parentElement.remove();
-  }
-  if (e.target.classList.contains("fa-pen-to-square")) {
-    var li = e.target.parentElement;
-    var inputValue = li.querySelector(".text").textContent;
-    li.classList.add("row");
-    li.classList.remove("list-item");
-    li.innerHTML = `
+function editTask(li) {
+  var inputValue = li.querySelector(".text").textContent;
+  li.classList.add("row");
+  li.classList.remove("list-item");
+  li.innerHTML = `
     <input type="text" class="input-box" value="${inputValue}" placeholder="Update Task">
     <button class="add-btn">Add Task</button>`;
+}
+
+function updateTask(li) {
+  var input = li.querySelector(".input-box");
+  var inputValue = input.value.trim();
+  if (!inputValue) {
+    input.focus();
+    return;
   }
-  if (e.target.classList.contains("add-btn")) {
-    var li = e.target.parentElement;
-    var inputValue = li.querySelector(".input-box").value;
-    li.classList.add("list-item");
-    li.classList.remove("row");
-    li.innerHTML = `
+  li.classList.add("list-item");
+  li.classList.remove("row");
+  li.innerHTML = `
     <span class="text">${inputValue}</span>
     <i class="fa-solid fa-pen-to-square edit-btn"></i>
     <i class="fa-solid fa-trash delete-btn"></i>`;
-  }
-  e.target.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      var li = e.target.parentElement;
-      var inputValue = li.querySelector(".input-box").value;
+}
 
-      li.classList.add("list-item");
-      li.classList.remove("row");
-      if (
-        inputValue === "" ||
-        inputValue === null ||
-        inputValue === undefined
-      ) {
-        li.innerHTML = `
-        <span class="text"></span>
-        <i class="fa-solid fa-pen-to-square edit-btn"></i>
-        <i class="fa-solid fa-trash delete-btn"></i>`;
-      } else {
-        li.innerHTML = `
-        <span class="text">${inputValue}</span>
-        <i class="fa-solid fa-pen-to-square edit-btn"></i>
-        <i class="fa-solid fa-trash delete-btn"></i>`;
-      }
-    }
+function deleteTask(li) {
+  li.remove();
+}
+
+btnAdd.addEventListener("click", addTask);
+inputBox.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") addTask(e);
+});
+
+listContainer.addEventListener("click", function (e) {
+  var childEl = e.target;
+  var li = childEl.parentElement;
+  if (childEl.classList.contains("fa-trash")) {
+    deleteTask(li);
+  } else if (childEl.classList.contains("fa-pen-to-square")) {
+    editTask(li);
+  } else if (childEl.classList.contains("add-btn")) {
+    updateTask(li);
+  }
+  childEl.addEventListener("keyup", function (e) {
+    if (e.key === "Enter") updateTask(li);
   });
 });
